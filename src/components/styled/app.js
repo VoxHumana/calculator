692a7b7a1b9a53f6a2048797b7083350891ea183
Oliver Liu <spaghettarian+git@gmail.com>
2018-05-06 01:19:17 -0700

    Fixed root app and screen CSS

--- src/components/styled/app.js
+++ src/components/styled/app.js
@@ -4,6 +4,5 @@ export default styled.div`
   display: grid;
   grid-template-rows: 1fr 5fr;
   grid-row-gap: 1px;
-  background-color: red;
   justify-items: center;
 `
--- src/components/styled/screen.js
+++ src/components/styled/screen.js
@@ -2,11 +2,16 @@ import styled from 'styled-components'
 
 const StyledScreen = styled.div`
   border-style: dashed;
+  border-color: lightblue;
+  border-radius: 8px;
+  border-width: 4px;
   padding: 4px 8px;
   margin: 2px;
   font-size: 32px;
   min-width: 246px;
-  display: inline-block;
+  background-color: lightseagreen;
+  text-align: right;
+  color: white;
 `
 
 export default StyledScreen
